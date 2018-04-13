b50eeabac01d0c1b7adc7662ef53c18fe763ca4b
Agalbreath <asgalb15@gmail.com>
2018-04-12 20:51:38 -0700

    Add emergency notification function, update notes

--- cgmSimulator.py
+++ cgmSimulator.py
@@ -18,3 +18,24 @@ def checkBloodSugar(currentBloodSugar):
 		currentState = -1
 	else:
 		currentState = 0
+
+def emergency(state):
+	if state == 1:
+		danger = "hyperglycemia"
+	elif state == 2:
+		danger = "hypoglycemia"
+	elif state == 3:
+		danger = "labile glycemic levels"
+	else:
+		danger = None
+	if danger = None: #If the user has gone for 2 straight hours with a healthy blood sugar range.
+		print("Healthy levels have been maintained consistently. \n Simulation complete. Have a nice day!")
+	else:
+		print(User is in danger from ")
+		print danger
+		urgent = input(". Contacting emergency contacts . . . \n Contacts reached. Call 911?)
+		if urgent == 1:
+			print("911 has been called. Please wait while the ambulance is dispatched to your location.")
+			print("\n \n Simulation complete. Have a nice day!")
+		else:
+			print("Simulation complete. Have a nice day!")

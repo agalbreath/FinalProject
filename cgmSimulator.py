6377078e4ee474bc7cd3290aee95d5a02fb772e1
Agalbreath <asgalb15@gmail.com>
2018-04-13 17:25:08 -0700

    Edit functions, add simulator proper

--- cgmSimulator.py
+++ cgmSimulator.py
@@ -1,8 +1,12 @@
+# This function is used to set the acceptable variance from normal ranges. This function defaults to 5 mg/dL
+# This will be the first function executed in simulator.py and will generally use the default value.
 def setThreshold(thresholdValue):
-	if thresholdValue = None:
+	if thresholdValue == None:	#If no value is put into the function, go to default.
 		threshold = 5;
-	else: 
-		threshold = thresholdValue;
+	else:
+		threshold = thresholdValue;	#Otherwise, use the given value.
+
+# This function is used to set the maximum acceptable blood sugar level, in mg/dL
 
 def setHealthyRange(eating):
 	if eating:
@@ -10,7 +14,7 @@ def setHealthyRange(eating):
 	else:
 		maxSafeLevel = 130;
 	minSafeLevel = 80;
-	
+
 def checkBloodSugar(currentBloodSugar):
 	if currentBloodSugar - 5 > maxSafeLevel:
 		currentState = 1
@@ -28,14 +32,19 @@ def emergency(state):
 		danger = "labile glycemic levels"
 	else:
 		danger = None
-	if danger = None: #If the user has gone for 2 straight hours with a healthy blood sugar range.
+	if danger == None: #If the user has gone for 2 straight hours with a healthy blood sugar range.
 		print("Healthy levels have been maintained consistently. \n Simulation complete. Have a nice day!")
+		running = False
 	else:
-		print(User is in danger from ")
-		print danger
-		urgent = input(". Contacting emergency contacts . . . \n Contacts reached. Call 911?)
-		if urgent == 1:
+		print("User is in danger from ")
+		print (danger)
+		urgent = input(". Contacting emergency contacts . . . \n Contacts reached. Call 911?")
+		if urgent == "yes" or urgent == "Yes" or urgent == "YES":
 			print("911 has been called. Please wait while the ambulance is dispatched to your location.")
 			print("\n \n Simulation complete. Have a nice day!")
+			running = False
 		else:
 			print("Simulation complete. Have a nice day!")
+			running = False
+
+#def compareBloodSugars(currentBloodSugar, pastBloodSugar):
---
+++ simulator.py
@@ -0,0 +1,9 @@
+import cgmSimulator
+
+print ("Welcome to CGM Simulator!")
+print("This simulator is used to demonstrate the functionality of Bluetooth-enabled Constant Glucose Monitors to automatically alert emergency contacts in case of emergency.")
+print ("During this simulator, you will be asked to input your blood sugar periodically.")
+print ("This will be counted as your blood sugar for the next ten minutes.")
+print ("If you can go for 2 hours with a healthy blood sugar range, the simulation will end.")
+print("If it remains above or below healthy ranges for 1 hour, your emergency contacts will be notified.")
+print ("You may also be asked to answer yes or no questions. Please type your answers as 'yes' or 'no'.")

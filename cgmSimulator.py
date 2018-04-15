23d0c5bb74249e8cebd5ce81291caf0078437f3e
Agalbreath <asgalb15@gmail.com>
2018-04-14 20:34:27 -0700

    Fix errors

--- cgmSimulator.py
+++ cgmSimulator.py
@@ -1,71 +1,73 @@
-def init():
-    global timer
-    global healthyTimer
-    timer = 0
-    healthyTimer = 0
 # This function is used to set the acceptable variance from normal ranges. This function defaults to 5 mg/dL
 # This will be the first function executed in simulator.py and will generally use the default value.
 def setThreshold(thresholdValue):
-	if thresholdValue == None:	#If no value is put into the function, go to default.
-		threshold = 5;
-	else:
-		threshold = thresholdValue;	#Otherwise, use the given value.
+    global timer
+    global healthyTimer
+    global threshold
+    timer = 0
+    healthyTimer = 0
+    threshold = thresholdValue;	#Otherwise, use the given value.
+
 
 # This function is used to set the maximum acceptable blood sugar level, in mg/dL
 # If the patient has gone for more than 2 hours without eating, the maximum blood sugar is lower.
 def setHealthyRange(eating):
     global maxSafeLevel
     global minSafeLevel
-    if eating == True:    
+    if eating == True:
         maxSafeLevel = 180;
     elif eating == False:
-	maxSafeLevel = 130;
+        maxSafeLevel = 130;
     minSafeLevel = 80;
 
 def checkBloodSugar(currentBloodSugar):
     global currentState
-    if currentBloodSugar - 5 > maxSafeLevel:	#The user is hyperglycemic
-	currentState = 1
-    elif currentBloodSugar + 5 < minSafeLevel:	#The user is hypoglycemic
-	currentState = -1
+    if currentBloodSugar - 5 > maxSafeLevel:
+        currentState = 1
+    elif currentBloodSugar + 5 < minSafeLevel:
+        currentState = -1
     else:
-	currentState = 0
+        currentState = 0
 
 def emergency(state):
     global running
     running = True
     if state == 1:
-	danger = "hyperglycemia"
+        danger = "hyperglycemia"
     elif state == -1:
-	danger = "hypoglycemia"
+        danger = "hypoglycemia"
     elif state == 3:
-	danger = "labile glycemic levels"
+        danger = "labile glycemic levels"
     else:
-	danger = None
-    if danger == None: #If the user has gone for 2 straight hours with a healthy blood sugar range.
-	print("Healthy levels have been maintained consistently. \n Simulation complete. Have a nice day!")
-	running = False
+        danger = None
+    if danger == None:
+        print("Healthy levels have been maintained consistently. \n Simulation complete. Have a nice day!")
+        running = False
     else:
-	print("User is in danger from ")
-	print (danger)
-	urgent = input(". Contacting emergency contacts . . . \n Contacts reached. Call 911?")
-	if urgent == "yes" or urgent == "Yes" or urgent == "YES":
-	    print("911 has been called. Please wait while the ambulance is dispatched to your location.")
-	    print("\n \n Simulation complete. Have a nice day!")
-	    running = False
+        print("User is in danger from ")
+        print (danger)
+        urgent = input(". Contacting emergency contacts . . . \n Contacts reached. Call 911?")
+        if urgent == "yes" or urgent == "Yes" or urgent == "YES":
+            print("911 has been called. Please wait while the ambulance is dispatched to your location.")
+            print("\n \n Simulation complete. Have a nice day!")
+            running = False
         else:
-	    print("Simulation complete. Have a nice day!")
-	    running = False
+            print("Simulation complete. Have a nice day!")
+            running = False
 
-def compareBloodSugars(currentBloodSugar, pastBloodSugar):
+def compareBloodSugars(setCurrentBloodSugar, setPastBloodSugar):
+    global currentBloodSugar
+    global pastBloodSugar
+    currentBloodSugar = currentBloodSugar
+    pastBloodSugar = pastBloodSugar
     if checkBloodSugar(currentBloodSugar) == checkBloodSugar(pastBloodSugar) and currentState != 0:
-        global timer += 10
-        global healthyTimer = 0
+        timer += 10
+        healthyTimer = 0
     elif currentState == 0:
-        global timer = 0
-        global healthyTimer += 10
+        timer = 0
+        healthyTimer += 10
     else:
         emergency(3)
-    if global timer >= 60 or global healthyTimer >= 60:
+    if timer >= 60 or healthyTimer >= 60:
         emergency(currentState)
     pastBloodSugar = currentBloodSugar
--- simulator.py
+++ simulator.py
@@ -1,6 +1,6 @@
 import cgmSimulator
 
-cgmSimulator.init()
+cgmSimulator.setThreshold(5)
 cgmSimulator.running = True
 print ("Welcome to CGM Simulator!")
 print ("This simulator is used to demonstrate the functionality of Bluetooth-enabled Constant Glucose Monitors to automatically alert emergency contacts in case of emergency.")
@@ -9,22 +9,22 @@ print ("This will be counted as your blood sugar for the next ten minutes.")
 print ("If you can go for 2 hours with a healthy blood sugar range, the simulation will end.")
 print ("If it remains above or below healthy ranges for 1 hour, your emergency contacts will be notified.")
 print ("You may also be asked to answer yes or no questions. Please type your answers as 'yes' or 'no'.")
-eating = raw_input ("First thing's first: Has it been less than two hours since your last meal? ")
+eating = input ("First thing's first: Has it been less than two hours since your last meal? ")
 if eating == "yes" or eating == "Yes" or eating == "YES":
     cgmSimulator.setHealthyRange(True)
-    currentBloodSugar = 150
-    pastBloodSugar = 150
+    cgmSimulator.currentBloodSugar = 150
+    cgmSimulator.pastBloodSugar = 150
     print ("Maximum healthy blood sugar is 180 mg/dL. Current blood sugar is 150 mg/dL.")
 elif eating == 'no' or eating == 'No' or eating == 'NO':
     cgmSimulator.setHealthyRange(False)
-    currentBloodSugar = 105
-    pastBloodSugar = 150
+    cgmSimulator.currentBloodSugar = 105
+    cgmSimulator.pastBloodSugar = 150
     print ("Maximum healthy blood sugar is 130 mg/dL. Current blood sugar is 105 mg/dL.")
 else:
-    print ('Well how is that possible? Is something wrong?')
-    print ('Error. Simulation ending.')
+    print ("Well how is that possible? Is something wrong?")
+    print ("Error. Simulation ending.")
     cgmSimulator.running = False
 while cgmSimulator.running == True:
-    cgmSimulator.currentBloodSugar = input("Please enter your current blood sugar: ")
-    cgmSimulator.checkBloodSugar(currentBloodSugar)
-    cgmSimulator.compareBloodSugars(currentBloodSugar, pastBloodSugar)
+    cgmSimulator.currentBloodSugar = int(input("Please enter your current blood sugar: "))
+    cgmSimulator.checkBloodSugar(cgmSimulator.currentBloodSugar)
+    cgmSimulator.compareBloodSugars(cgmSimulator.currentBloodSugar, cgmSimulator.pastBloodSugar)

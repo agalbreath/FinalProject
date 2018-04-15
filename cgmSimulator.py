f57c956750c1c592fe3afc7f56540a2005d4c5c5
Aidan <asgalb15@gmail.com>
2018-04-14 18:41:43 -0700

    Make edits, testing required

--- cgmSimulator.py
+++ cgmSimulator.py
@@ -9,36 +9,39 @@ def setThreshold(thresholdValue):
 # This function is used to set the maximum acceptable blood sugar level, in mg/dL
 # If the patient has gone for more than 2 hours without eating, the maximum blood sugar is lower.
 def setHealthyRange(eating):
-	if eating:
-		maxSafeLevel = 180;
-	else:
-		maxSafeLevel = 130;
-	minSafeLevel = 80;
+    global maxSafeLevel
+    global minSafeLevel
+    if eating == True:    
+        maxSafeLevel = 180;
+    elif eating == False:
+	maxSafeLevel = 130;
+    minSafeLevel = 80;
 
 def checkBloodSugar(currentBloodSugar):
-	if currentBloodSugar - 5 > maxSafeLevel:	#The user is hyperglycemic
-		currentState = 1
-	elif currentBloodSugar + 5 < minSafeLevel:	#The user is hypoglycemic
-		currentState = -1
-	else:
-		currentState = 0
+    global currentState
+    if currentBloodSugar - 5 > maxSafeLevel:	#The user is hyperglycemic
+	currentState = 1
+    elif currentBloodSugar + 5 < minSafeLevel:	#The user is hypoglycemic
+	currentState = -1
+    else:
+	currentState = 0
 
 def emergency(state):
-	if state == 1:
-		danger = "hyperglycemia"
-	elif state == -1:
-		danger = "hypoglycemia"
-	elif state == 3:
-		danger = "labile glycemic levels"
-	else:
-		danger = None
-	if danger == None: #If the user has gone for 2 straight hours with a healthy blood sugar range.
-		print("Healthy levels have been maintained consistently. \n Simulation complete. Have a nice day!")
-		running = False
-	else:
-		print("User is in danger from ")
-		print (danger)
-		urgent = input(". Contacting emergency contacts . . . \n Contacts reached. Call 911?")
+    if state == 1:
+	danger = "hyperglycemia"
+    elif state == -1:
+	danger = "hypoglycemia"
+    elif state == 3:
+	danger = "labile glycemic levels"
+    else:
+	danger = None
+    if danger == None: #If the user has gone for 2 straight hours with a healthy blood sugar range.
+	print("Healthy levels have been maintained consistently. \n Simulation complete. Have a nice day!")
+	running = False
+    else:
+	print("User is in danger from ")
+	print (danger)
+	urgent = input(". Contacting emergency contacts . . . \n Contacts reached. Call 911?")
 		if urgent == "yes" or urgent == "Yes" or urgent == "YES":
 			print("911 has been called. Please wait while the ambulance is dispatched to your location.")
 			print("\n \n Simulation complete. Have a nice day!")
--- simulator.py
+++ simulator.py
@@ -8,19 +8,19 @@ print ("This will be counted as your blood sugar for the next ten minutes.")
 print ("If you can go for 2 hours with a healthy blood sugar range, the simulation will end.")
 print ("If it remains above or below healthy ranges for 1 hour, your emergency contacts will be notified.")
 print ("You may also be asked to answer yes or no questions. Please type your answers as 'yes' or 'no'.")
-eating = ("First thing's first: Has it been less than two hours since your last meal? ")
+eating = raw_input ("First thing's first: Has it been less than two hours since your last meal? ")
 if eating == "yes" or eating == "Yes" or eating == "YES":
-    setHealthyRange(True)
+    cgmSimulator.setHealthyRange(True)
     currentBloodSugar = 150
     pastBloodSugar = 150
     print ("Maximum healthy blood sugar is 180 mg/dL. Current blood sugar is 150 mg/dL.")
-else:
-    setHealthyRange(False)
+elif eating == 'no' or eating == 'No' or eating == 'NO':
+    cgmSimulator.setHealthyRange(False)
     currentBloodSugar = 105
     pastBloodSugar = 150
     print ("Maximum healthy blood sugar is 130 mg/dL. Current blood sugar is 105 mg/dL.")
-timer = 0
-while running < True:
-    currentBloodSugar = input("Please enter your current blood sugar: ")
-    checkBloodSugar(currentBloodSugar)
-    compareBloodSugars(currentBloodSugar, pastBloodSugar)
+cgmSimulator.timer = 0
+while running == True:
+    cgmSimulator.currentBloodSugar = input("Please enter your current blood sugar: ")
+    cgmSimulator.checkBloodSugar(currentBloodSugar)
+    cgmSimulator.compareBloodSugars(currentBloodSugar, pastBloodSugar)
